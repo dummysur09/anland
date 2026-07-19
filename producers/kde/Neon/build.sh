b8d6f7d833f3af4c2c0dc38051c4742eeab2a196
kk703338 <kk703338@gmail.com>
2026-07-19 14:19:19 +0530

    Update Neon build.sh to correct packaging names of libplasma-activities-dev and libplasma-dev

--- producers/kde/Neon/build.sh
+++ producers/kde/Neon/build.sh
@@ -129,12 +129,12 @@ build_pkg_deb() {
         libxcb-xinput-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev \
         libxcb1-dev libx11-dev hwdata libqaccessibilityclient-qt6-dev \
         gettext knighttime-dev kwayland-dev libcap2-bin libegl-dev \
-        libfontconfig-dev libfreetype-dev libkirigami-dev libplasmaactivities-dev \
+        libfontconfig-dev libfreetype-dev libkirigami-dev libplasma-activities-dev \
         libsystemd-dev libxcursor-dev libxi-dev pkgconf plasma-wayland-protocols \
         qt6-5compat-dev qt6-base-private-dev qt6-declarative-private-dev \
         qt6-sensors-dev qt6-svg-dev qt6-tools-dev qt6-wayland-private-dev \
         wayland-protocols xwayland libxcb-cursor-dev libxcb-image0-dev \
-        libxcb-util-dev libxcb-xtest0-dev pkg-kde-tools plasma-dev || true
+        libxcb-util-dev libxcb-xtest0-dev pkg-kde-tools libplasma-dev || true
 
     # Download the source
     log "Downloading source code for '${src}'"
