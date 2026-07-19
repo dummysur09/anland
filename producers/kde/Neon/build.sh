477e4a5e85942d073dcc4a62158c7e9826eb38ec
kk703338 <kk703338@gmail.com>
2026-07-19 14:30:56 +0530

    Update Neon build.sh to use libkf6activities-dev package name

--- producers/kde/Neon/build.sh
+++ producers/kde/Neon/build.sh
@@ -129,7 +129,7 @@ build_pkg_deb() {
         libxcb-xinput-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev \
         libxcb1-dev libx11-dev hwdata libqaccessibilityclient-qt6-dev \
         gettext knighttime-dev kwayland-dev libcap2-bin libegl-dev \
-        libfontconfig-dev libfreetype-dev libkirigami-dev libkf6plasmaactivities-dev \
+        libfontconfig-dev libfreetype-dev libkirigami-dev libkf6activities-dev \
         libsystemd-dev libxcursor-dev libxi-dev pkgconf plasma-wayland-protocols \
         qt6-5compat-dev qt6-base-private-dev qt6-declarative-private-dev \
         qt6-sensors-dev qt6-svg-dev qt6-tools-dev qt6-wayland-private-dev \
