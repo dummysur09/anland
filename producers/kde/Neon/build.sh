3a539c65148f304e0e314f6e2ed587a0e87b857f
kk703338 <kk703338@gmail.com>
2026-07-19 14:14:15 +0530

    Update Neon build.sh to pre-install pkg-kde-tools and plasma-dev packaging helpers manually

--- producers/kde/Neon/build.sh
+++ producers/kde/Neon/build.sh
@@ -134,7 +134,7 @@ build_pkg_deb() {
         qt6-5compat-dev qt6-base-private-dev qt6-declarative-private-dev \
         qt6-sensors-dev qt6-svg-dev qt6-tools-dev qt6-wayland-private-dev \
         wayland-protocols xwayland libxcb-cursor-dev libxcb-image0-dev \
-        libxcb-util-dev libxcb-xtest0-dev || true
+        libxcb-util-dev libxcb-xtest0-dev pkg-kde-tools plasma-dev || true
 
     # Download the source
     log "Downloading source code for '${src}'"
